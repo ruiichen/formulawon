5b0c6ff82e45bf37e3fff62fd2fe7e54d1d69443
ruiichen <frankchen99999@gmail.com>
2024-09-18 20:41:03 -0400

    - changes for API

--- pytorched/app.py
+++ pytorched/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+import json
+import pandas as pd
+import torch
+from flask import Flask, jsonify
+from model import F1RacePrediction
+from util.predictor import predict_winner_from_pole, predict_winner_from_quali
 app = Flask(__name__)
+_data = pd.read_csv('data/processed_data.csv')
+_model = F1RacePrediction()
+_model.load_state_dict(torch.load('racemodel.pth', weights_only=True))
 
+@app.route('/predictquali/<season>/<round>', methods=['GET'])
+def predict_quali(season, round):
+    return jsonify({'winner': predict_winner_from_quali(season, round, _model, _data)})
 
-@app.route('/predictwinner', methods=['GET'])
-def predict_winner():
-    return 'Hello World!'
+@app.route('/predictpole/<season>/<round>', methods=['GET'])
+def predict_pole(season, round):
+    return jsonify({'win': True if predict_winner_from_pole(season, round, _model, _data) else False})
 
 if __name__ == '__main__':
     app.run()
