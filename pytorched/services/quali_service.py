2540f0e73d898f8d1ad4b90ebb4c939265326bc6
ruiichen <frankchen99999@gmail.com>
2024-09-21 20:34:50 -0400

    - bugfixes for current round

--- pytorched/services/quali_service.py
+++ pytorched/services/quali_service.py
@@ -4,14 +4,20 @@ from dateutil.relativedelta import *
 from util.consts import headers, booleans
 from util.exceptions import NotFoundError, ExternalServiceError
 
+_quali = None
 
 def get_quali(season, round):
+    global _quali
+
     qualifying_results = {'grid': [],
                           'driver': [],
                           'constructor': [],
                           'qualifying_time': [],
                           'season': [],
-                          'round': []}
+                          'round': [],
+                          'nationality': [],
+                          'date_of_birth': []
+                          }
 
     url = 'http://ergast.com/api/f1/{}/{}/qualifying.json'
     try:
@@ -26,6 +32,8 @@ def get_quali(season, round):
         qualifying_results['grid'].append(int(item['position']))
         qualifying_results['driver'].append(item['Driver']['driverId'])
         qualifying_results['constructor'].append(item['Constructor']['constructorId'])
+        qualifying_results['nationality'].append(item['Driver']['nationality'])
+        qualifying_results['date_of_birth'].append(item['Driver']['dateOfBirth'])
         try:
             if 'Q3' in item and item['Q3']:
                 qualifying_results['qualifying_time'].append(item['Q3'])
@@ -40,40 +48,61 @@ def get_quali(season, round):
             qualifying_results['Q'].append(int(1))
         qualifying_results['season'].append(int(json['MRData']['RaceTable']['Races'][0]['season']))
         qualifying_results['round'].append(int(json['MRData']['RaceTable']['Races'][0]['round']))
+    _quali = qualifying_results
     return  pd.DataFrame(qualifying_results)
 
 def get_drivers(season, round):
+    global _quali
     driver_standings = {'season': [],
                         'round': [],
                         'driver': [],
                         'driver_points': [],
                         'driver_wins': [],
-                        'driver_standings_pos': [],
-                        'nationality': [],
-                        'date_of_birth': []}
+                        'driver_standings_pos': []}
 
     url = 'https://ergast.com/api/f1/{}/{}/driverStandings.json'
-    try:
-        resp = requests.get(url.format(season, round))
-    except:
-        raise ExternalServiceError()
-    json = resp.json()
-
-    for item in json['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']:
-        driver_standings['season'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['season']))
-        driver_standings['round'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['round']))
-        driver_standings['driver'].append(item['Driver']['driverId'])
-        driver_standings['nationality'].append(item['Driver']['nationality'])
-        driver_standings['date_of_birth'].append(item['Driver']['dateOfBirth'])
+    if int(round) == 1:
+        driver_standings = {
+            'season': _quali["season"],
+            'round': _quali["round"],
+            'driver': _quali["driver"],
+            'driver_points': [0 for x in _quali["season"]],
+            'driver_wins': [0 for x in _quali["season"]],
+            'driver_standings_pos': [1 for x in _quali["season"]]
+        }
+    else:
         try:
-            driver_standings['driver_points'].append(int(item['points']))
+            round = int(round) - 1
+            resp = requests.get(url.format(season, round))
         except:
-            driver_standings['driver_points'].append(0)
-        driver_standings['driver_wins'].append(int(item['wins']))
-        driver_standings['driver_standings_pos'].append(int(item['position']))
+            raise ExternalServiceError()
+        json = resp.json()
+
+        for item in json['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']:
+            if item['Driver']['driverId'] not in _quali["driver"]:
+                continue
+            driver_standings['season'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['season']))
+            driver_standings['round'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['round']) + 1)
+            driver_standings['driver'].append(item['Driver']['driverId'])
+            try:
+                driver_standings['driver_points'].append(int(item['points']))
+            except:
+                driver_standings['driver_points'].append(0)
+            driver_standings['driver_wins'].append(int(item['wins']))
+            driver_standings['driver_standings_pos'].append(int(item['position']))
+
+        for driver in _quali["driver"]:
+            if driver not in driver_standings['driver']:
+                driver_standings['season'].append(int(_quali["season"][0]))
+                driver_standings['round'].append(int(_quali["round"][0]))
+                driver_standings['driver'].append(driver)
+                driver_standings['driver_points'].append(0)
+                driver_standings['driver_wins'].append(0)
+                driver_standings['driver_standings_pos'].append(20)
     return  pd.DataFrame(driver_standings)
 
 def get_constructors(season, round):
+    global _quali
     constructor_standings = {'season': [],
                              'round': [],
                              'constructor': [],
@@ -82,22 +111,35 @@ def get_constructors(season, round):
                              'constructor_standings_pos': []}
 
     url = 'https://ergast.com/api/f1/{}/{}/constructorStandings.json'
-    try:
-        resp = requests.get(url.format(season, round))
-    except:
-        raise ExternalServiceError()
-    json = resp.json()
-
-    for item in json['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings']:
-        constructor_standings['season'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['season']))
-        constructor_standings['round'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['round']))
-        constructor_standings['constructor'].append(item['Constructor']['constructorId'])
+    if int(round) == 1:
+        unique = set(_quali["constructor"])
+        unique = list(unique)
+        constructor_standings = {
+            'season': [_quali["season"][0] for x in unique],
+            'round': [_quali["round"][0] for x in unique],
+            'constructor': unique,
+            'constructor_points': [0 for x in unique],
+            'constructor_wins': [0 for x in unique],
+            'constructor_standings_pos': [1 for x in unique]
+        }
+    else:
         try:
-            constructor_standings['constructor_points'].append(int(item['points']))
+            round = int(round) - 1
+            resp = requests.get(url.format(season, round))
         except:
-            constructor_standings['constructor_points'].append(0)
-        constructor_standings['constructor_wins'].append(int(item['wins']))
-        constructor_standings['constructor_standings_pos'].append(int(item['position']))
+            raise ExternalServiceError()
+        json = resp.json()
+
+        for item in json['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings']:
+            constructor_standings['season'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['season']))
+            constructor_standings['round'].append(int(json['MRData']['StandingsTable']['StandingsLists'][0]['round']) + 1)
+            constructor_standings['constructor'].append(item['Constructor']['constructorId'])
+            try:
+                constructor_standings['constructor_points'].append(int(item['points']))
+            except:
+                constructor_standings['constructor_points'].append(0)
+            constructor_standings['constructor_wins'].append(int(item['wins']))
+            constructor_standings['constructor_standings_pos'].append(int(item['position']))
     return  pd.DataFrame(constructor_standings)
 
 def get_race(season, round):
@@ -128,11 +170,9 @@ def get_quali_session(season, round):
     driver = get_drivers(season, round)
     constructors = get_constructors(season, round)
     race = get_race(season, round)
-
     df1 = pd.merge(race, driver, how='left', on=['season', 'round'])
     df2 = pd.merge(df1, quali, how='left', on=['season', 'round', 'driver'])
     final_df = pd.merge(df2, constructors, how='inner', on=['season', 'round', 'constructor'])
-
     final_df['date'] = pd.to_datetime(final_df.date)
     final_df['date_of_birth'] = pd.to_datetime(final_df.date_of_birth)
     final_df['age'] = final_df.apply(lambda x: relativedelta(x['date'], x['date_of_birth']).years, axis=1)
